ebb51ce86a5ba1dfa49feab62147b2f42336f980
Gladys Chang <gladys@gladys-changs-macbook-pro.local>
2013-10-20 13:50:01 -0400

    Add cancel button.  Begin putting in login info.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,9 @@ Testapp1::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Action mailer to know where to send the mailers from
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Testapp1::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #http://firehosenycgc.herokuapp.com/
+  config.action_mailer.default_url_options = { :host => 'firehosenycgc.herokuapp.com' }
+
 end
